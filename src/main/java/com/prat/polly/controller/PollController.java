f9e3ef3c95e3da2d3cb76a16767f0f90dd74fccd
pdeshmukh <pdeshmukh@mediaocean.com>
2020-12-28 16:55:37 +0530

    Allowing cross origin to hit controllers

--- src/main/java/com/prat/polly/controller/PollController.java
+++ src/main/java/com/prat/polly/controller/PollController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/poll")
 public class PollController {
